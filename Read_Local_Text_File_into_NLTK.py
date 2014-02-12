c7e09f5fa80c599339a7986ab79454a0cae5d615
xbsd <xbsd@yahoo.com>
2014-02-12 08:35:27 -0500

    Create Read_Local_Text_File_into_NLTK.py

---
+++ Read_Local_Text_File_into_NLTK.py
@@ -0,0 +1,10 @@
+>>> f = open("AChristmasCarol.txt")
+>>> raw = f.read()
+>>> tokens = nltk.word_tokenize(raw)
+>>> words = [w.lower() for w in tokens]
+>>> words[:20]
+['title', ':', 'a', 'christmas', 'carol', 'author', ':', 'charles', 'dickens', 'illustrator', ':', 'arthur', 'rackham', 'release', 'date', ':', 'december', '24', ',', '2007']
+>>> vocab = sorted(set(words))
+>>> text = nltk.Text(tokens)
+>>> text[:20]
+['Title', ':', 'A', 'Christmas', 'Carol', 'Author', ':', 'Charles', 'Dickens', 'Illustrator', ':', 'Arthur', 'Rackham', 'Release', 'Date', ':', 'December', '24', ',', '2007']
